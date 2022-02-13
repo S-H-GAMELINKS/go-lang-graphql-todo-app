31ec988f2025f8d0829c803a84fda0f35c4408b5
S-H-GAMELINKS <gamelinks007@gmail.com>
2022-02-13 12:01:52 +0900

    Fix argument type

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRoutes(router *gin.Router) {
+func SetupRoutes(router *gin.Engine) {
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
