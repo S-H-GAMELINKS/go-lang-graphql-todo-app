e8d0e3ab59efdcef3e58fd857cf6ef9770d04e4c
S-H-GAMELINKS <gamelinks007@gmail.com>
2022-02-13 18:15:54 +0900

    Add GraphQL endpoint & GraphQL Playground endpoint

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -2,15 +2,28 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/99designs/gqlgen/handler"
+	"github.com/S-H-GAMELINKS/go-lang-graphql-todo-app/api/graph"
+	"github.com/S-H-GAMELINKS/go-lang-graphql-todo-app/api/graph/generated"
 )
 
-func SetupRoutes(router *gin.Engine) {
-	api := router.Group("api")
-	{
-		api.GET("/ping", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "pong",
-			})
-		})	
+func graphqlHandler() gin.HandlerFunc {
+	h := handler.GraphQL(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
 	}
+}
+
+func graphqlPlaygroundHandler() gin.HandlerFunc {
+	h := handler.Playground("GraphQL", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func SetupRoutes(router *gin.Engine) {
+	router.POST("/query", graphqlHandler())
+	router.GET("/playground", graphqlPlaygroundHandler())
 }
