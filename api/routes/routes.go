5ae26b5ea6d0af9bc892e166329267c79782825d
S-H-GAMELINKS <gamelinks007@gmail.com>
2022-02-13 12:07:05 +0900

    Using group for routing

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -5,9 +5,12 @@ import (
 )
 
 func SetupRoutes(router *gin.Engine) {
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	api := router.Group("api")
+	{
+		api.GET("/ping", func(c *gin.Context) {
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
+		})	
+	}
 }
